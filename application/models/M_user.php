8745b2f7d762164446a0acaad1d295ded987499a
Syahrul Ramadhan <58390540+syahrullrmdhn@users.noreply.github.com>
2025-06-25 10:06:19 +0700

    Fix M_user model with get and update methods

--- application/models/M_user.php
+++ application/models/M_user.php
@@ -4,8 +4,14 @@ class M_user extends CI_Model {
     public function get_all() {
         return $this->db->order_by('username')->get($this->table)->result();
     }
+    public function get($id) {
+        return $this->db->where('id', $id)->get($this->table)->row();
+    }
     public function insert(array $d) {
         return $this->db->insert($this->table,$d);
+    }
+    public function update($id, array $d) {
+        return $this->db->where('id', $id)->update($this->table, $d);
     }
     public function delete($id) {
         return $this->db->delete($this->table,['id'=>$id]);
